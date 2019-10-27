4e17597a1a5539515172a149c5ad6721a04e8549
Emyebong <emyebong@gmail.com>
2019-10-27 21:48:03 +0100

    Completed module

---
+++ src/app/product.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+
+
+describe('ProductService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ProductService]
+    });
+  });
+
+  it('should be created', inject([ProductService], (service: ProductService) => {
+    expect(service).toBeTruthy();
+  }));
+});
